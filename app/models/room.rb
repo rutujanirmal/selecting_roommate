8c9f10ab4674b74f98e55196cc41b113f1bc4357
Niharika Bhoi <59463882+niharikabhoi2020@users.noreply.github.com>
2022-09-21 12:03:11 +0530

    Revert "Db/rn/create user and room model"

--- app/models/room.rb
+++
@@ -1,7 +0,0 @@
-class Room < ApplicationRecord
-  has_many :users
-
-  validates :booked_by, presence: true
-  validates :roommates, presence: true
-  
-end
--- config/initializers/constants.rb
+++
@@ -1,2 +0,0 @@
-EMAIL_REGEX = /^[a-z.]+\@joshsoftware\.(com|digital)$/i
-GENDER_REGEX = /^(M|F)$/
